f59130157d504ffba275a1d19e6b3d391f9c69f0
Micah Warren <micahw@lordnull.com>
2013-05-14 23:41:52 -0500

    Added many tests

--- test/gen_websocket_tests.erl
+++ test/gen_websocket_tests.erl
@@ -43,6 +43,139 @@ communication_test_() ->
 			timer:sleep(1000),
 			Got2 = ws_server:reset_msgs(Handler),
 			?assertEqual([Msg], Got2)
-		end}
+		end},
+
+		{"can swap mode to active: once", fun() ->
+			Got = gen_websocket:setopts(WS, [{active, once}]),
+			?assertEqual(ok, Got)
+		end},
+
+		{"can swap mode to active: true", fun() ->
+			Got = gen_websocket:setopts(WS, [{active, true}]),
+			?assertEqual(ok, Got)
+		end},
+
+		{"can swap mode to passive (active: false)", fun() ->
+			Got = gen_websocket:setopts(WS, [{active, false}]),
+			?assertEqual(ok, Got)
+		end},
+
+		{"passive mode tests", setup, fun() ->
+			gen_websocket:setopts(WS, [{active, false}])
+		end, fun(_) ->
+			ok
+		end,
+		fun(_) -> [
+
+			{"passive receive a frame", fun() ->
+				Msg = <<"I'm a little teapot">>,
+				ws_server:send(Msg),
+				Got = gen_websocket:recv(WS),
+				?assertEqual({ok, {text, Msg}}, Got)
+			end},
+
+			{"passive multiple recvs", fun() ->
+				Msgs = [{text, M} || M <- [<<"sending1">>, <<"sending2">> ,<<"sending3">>]],
+				[ws_server:send(M) || M <- Msgs],
+				Got = [gen_websocket:recv(WS) || _ <- lists:seq(1,3)],
+				?assertEqual(Msgs, Got)
+			end},
+
+			{"passive timeouts", fun() ->
+				Got = gen_websocket:recv(WS, 1000),
+				?assertEqual({error, timeout}, Got)
+			end},
+
+			{"no active message while passive", fun() ->
+				Msg = <<"this is beckmen">>,
+				ws_server:send(Msg),
+				Got = receive
+					{gen_websocket, WS, SocketData} ->
+						{error, SocketData}
+				after 1000 ->
+					timeout
+				end,
+				?assertEqual(timeout, Got)
+			end}
+
+		] end},
+
+		{"active once mode tests", setup, fun() ->
+			gen_websocket:setopts(WS, [{active, once}])
+		end,
+		fun(_) ->
+			ok
+		end,
+		fun(_) -> [
+
+			{"cannot do passive recv", fun() ->
+				Got = gen_websocket:recv(WS, 1000),
+				?assertEqual({error, not_passive}, Got)
+			end},
+
+			{"gets a message", fun() ->
+				Msg = <<"justice league, assemble!">>,
+				ws_server:send(Msg),
+				Got = receive
+					{gen_websocket, WS, {text, Msg}} ->
+						ok
+				after 1000 ->
+					timeout
+				end,
+				?assertEqual(ok, Got)
+			end},
+
+			{"now in passive mode", fun() ->
+				Got = gen_websocket:recv(WS, 1000),
+				?assertEqual({error, timeout}, Got)
+			end},
+
+			{"receive a bunch", fun() ->
+				Msgs = [{text, M} || M <- [<<"der thing 1">>, <<"der thing 2">>, <<"der thing 3">>]],
+				[ws_server:send(M) || M <- Msgs],
+				FoldFun = fun(_, Acc) ->
+					gen_websocket:setopts(WS, [{active, once}]),
+					receive
+						{gen_websocket, WS, Msg} ->
+							Acc ++ [Msg]
+					after 1000 ->
+						Acc ++ [{error, timeout}]
+					end
+				end,
+				Got = lists:foldl(FoldFun, [], Msgs),
+				?assertEqual(Msgs, Got)
+			end}
+
+		] end},
+
+		{"active mode tests", setup, fun() ->
+			gen_websocket:setopts(WS, [{active, true}])
+		end,
+		fun(_) ->
+			ok
+		end,
+		fun(_) -> [
+
+			{"can't do recv", fun() ->
+				Got = gen_websocket:recv(WS),
+				?assertEqual({error, not_active}, Got)
+			end},
+
+			{"gets a bunch of messages", fun() ->
+				Msgs = [{text, M} || M <- [<<"vonder 1">>, <<"vonder 2">>, <<"vonder 3">>]],
+				[ws_server:send(M) || M <- Msgs],
+				FoldFun = fun(_, Acc) ->
+					receive
+						{gen_websocket, WS, M} ->
+							Acc ++ [M]
+					after 1000 ->
+						Acc ++ [{error, timeout}]
+					end
+				end,
+				Got = lists:foldl(FoldFun, [], Msgs),
+				?assertEqual(Msgs, Got)
+			end}
+
+		] end}
 
 	] end}.
